42bc8d26e02e90c8b4b72e7173a79caa78ebaa3f
scipalo <spela.copi@gmail.com>
2020-01-15 22:43:12 +0100

    Find betti numbers and detect shape.

--- homology_group.py
+++ homology_group.py
@@ -1,37 +1,58 @@
+import shape_detection
 import collections
+import ast
 
 def prave_betti(tabela_betties):
     tabela = [*zip(*tabela_betties)]
     dic_0 = isci_beti(tabela[0])
     dic_1 = isci_beti(tabela[1])
     dic_2 = isci_beti(tabela[2])
-    print(dic_0, '\n', dic_1, '\n', dic_2)
+    return ([dic_0,  dic_1,  dic_2])
 
 
 def isci_beti(sez):
     dic = {}
-    beti = 0
+    betti = sez[0]
     st = 1
-    X = collections.deque(sez)
+    X = collections.deque(sez[1:])
     while X:
         i = X.popleft()
         if i == betti:
             st += 1
         else:
-            if i in dic.keys:
-                dic[beti] = max(st, dic[i])
+            #print(betti, dic, st)
+            if betti in dic.keys():
+                dic[betti] = max(st, dic[betti])
             else:
-                dic[beti] = st
+                dic[betti] = st
             betti = i
             st = 1
-    return dic
+    if i in dic.keys():
+        dic[betti] = max(st, dic[i])
+    else:
+        dic[betti] = st
+    res = []
+    for j in dic.keys():
+        if round(dic[j]/len(sez), 2)>=0.3:
+            res.append(j)
+    return (max(res))
 
-# GET BETTI NUMBER ARRAYS
-m = 'circle_betti'
-with open("shape-detection/betti/"+m+"txt", 'w') as g:
-   for betti in betti_table:
-       g.write('{} {} {}'.format(betti(0), betti(1), betti(2)))
-g.close()
+def doloci_hom_group(filename):
+    table = []
+    with open('betti/'+filename+'.txt', 'r') as f:
+        for line in f:
+            table.append(ast.literal_eval(line))
+    f.close()
+    print(prave_betti(table))
+    return(prave_betti(table))
 
-# DETECT SHAPES
-# detect shape
+#learning
+#circle_betti [1, 1, 0]
+#line_betti [1, 0, 0]
+#plane_betti [1, 0, 0]
+#sphere_betti [1, 0, 1]
+#torus_betti [1, 2, 1]
+
+#testing
+betti = doloci_hom_group('test_plane_betti')
+shape_detection.shape_detection('plane3', betti)
--- shape_detection.py
+++ shape_detection.py
@@ -1,5 +1,12 @@
+import vietoris_rips_complex
+import blender_import
+import boundary_matrix
 
-def shape_detection(name, betti, fileName):
+from numpy.linalg import norm
+from numpy import arange
+import numpy
+
+def shape_detection(filename, betti):
 
     # plane
     # line
@@ -7,27 +14,87 @@ def shape_detection(name, betti, fileName):
     # monkey
     if betti == [1,0,0]:
 
-        # avg razdalja med točkami
-        # max razdalja med točkami
 
-        # KROGLA
-        # radij = polovica max razdalje
-        # gostota = avg kompleksa
-        # središče = središčna točka kompleksa kompleksa
+        point_set = blender_import.import_native(filename)
+        point_trans = [*zip(*point_set)]
+        avg_point,dist = [],[]
 
-        # PRESEK
-        # odstrani vse točke, ki so blizu kompleksu
-        # d = avg razdala * 1,5?
+        for coordinate in point_trans:
+            dist.append(max(coordinate) - min(coordinate))
+            avg_point.append(sum(coordinate)/len(coordinate))
 
-        # poženi še enkrat voroni, betti
+        print("diff: ",dist)
 
-        intersection_betti = [0,0,0]
+       # min max razdalja
 
-        if intersection_betti == [1,2,0]:
-            print("line")
+        premer = max(dist)
+        for d in dist:
+            if d < premer and d != 0:
+                premer = d
 
-        if intersection_betti == [2,0,0]:
-            print("plane")
+
+        print('premer: ',premer)
+
+        # izberi povprečno točko
+
+        print('avg point: ',avg_point)
+
+        # izdelaj in skaliraj sfero
+
+        sphere_set = blender_import.import_native("enota2")
+        scaled_sphere = []
+
+        for point in sphere_set:
+            scaled = [point[i]*(0.25*premer) + avg_point[i] for i in range(len(point))]
+            scaled_sphere.append(scaled)
+
+        # določi presek
+
+        pointIter = [numpy.array(x) for x in point_set]
+        sphereIter = [numpy.array(x) for x in scaled_sphere]
+
+        intersect = []
+        for s in sphereIter:
+            add = True
+            for p in pointIter:
+                if norm(s - p) < premer * 0.125:
+                    add = False
+            if add == True:
+                s = [round(x,5) for x in tuple(s)]
+                intersect.append(tuple(s))
+
+        # pošlji ostanek preseka v run
+
+        print("Intersect")
+        #for i in intersect:
+        #    print(i)
+
+        min_dist = min(norm(x-y) for x in sphereIter for y in sphereIter if not numpy.array_equal(x,y))
+
+        # epsilon < premer * 0.125
+        # epsilon > min razdalje + malo
+        detected = ""
+
+        for epsilon in arange( min_dist + 0.1*min_dist, premer * 0.125,0.01):
+            print(epsilon)
+            vr = vietoris_rips_complex.vietoris_rips_native(intersect,epsilon, False)
+            print("len: ",len(vr))
+            betti = boundary_matrix.betti_direkt(vr)
+            print("betti", betti)
+
+            if betti == [0,0,0]:
+                detected = "ball"
+
+            elif betti == [1,2,0]:
+                detected = "line"
+
+            elif betti == [2,0,0]:
+                detected = "plane"
+
+            else:
+                "WWFTRTTFTFTFTFTFTTFTFTF"
+
+        print(detected)
 
     # circle
     elif betti == [1, 1, 0]:
@@ -42,11 +109,7 @@ def shape_detection(name, betti, fileName):
     elif betti == [1,2,1]:
         print("torus")
 
-
-
-
-
-names = ["plane", "line", "circle", "sphere", "torus"]
+names = ["plane3", "line3", "circle", "sphere", "torus"]
 bettis = [[1,0,0],[1,0,0],[1,1,0],[1,0,1],[1,2,1]]
-for betti,name in zip(bettis,names):
-    shape_detection(name, betti)
+#for betti,name in zip(bettis,names):
+#shape_detection(names[0], bettis[0])
